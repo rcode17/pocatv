ac981e5193d3f3013e2d9b38932d341022993596
rcode17 <rickyZI470@gmail.com>
2025-03-04 16:52:34 -0500

    first commit

---
+++ src/main/java/com/bancolombia/atv/AtvApplication.java
@@ -0,0 +1,13 @@
+package com.bancolombia.atv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvApplication.class, args);
+	}
+
+}
